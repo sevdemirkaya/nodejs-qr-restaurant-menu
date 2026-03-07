ad7ad3c1d3a83d6eada0230d0ac22b615b126723
sevdemirkaya <sevdemirkaya@icloud.com>
2026-03-07 17:37:26 +0300

    first commit - qr restaurant menu project

---
+++ index.js
@@ -0,0 +1,197 @@
+const express = require('express');
+const app = express();
+
+console.log("DOSYA CALISTI");
+
+let orders = [];
+
+app.use(express.json());
+app.use(express.static('public'));
+
+/* ---------------- MENU ---------------- */
+app.get('/menu', (req, res) => {
+    res.json([
+        {
+            category: "Sıcak İçecekler",
+            items: [
+ { id:1, name:"Espresso", price:120, image:"/images/espresso.jpeg" },
+ { id:2, name:"Filtre Kahve", price:130, image:"/images/filtercoffee.jpeg" },
+ { id:3, name:"Americano", price:135, image:"/images/americano.jpeg" },
+ { id:4, name:"Caffe Latte", price:150, image:"/images/cafelatte.jpeg" },
+ { id:5, name:"Flat White", price:155, image:"/images/flatwhite.jpeg" },
+ { id:6, name:"White Chocolate Mocha", price:170, image:"/images/whitechocolatemocha.jpeg" },
+ { id:7, name:"Türk Kahvesi", price:100, image:"/images/turkishcoffe.jpeg" }
+]
+        },
+        {
+    category: "Soğuk  İçecekler",
+    items: [
+        {id: 8, name: "Su", price:50, image:"/images/su.jpeg" },
+        { id: 9, name: "Iced Americano", price:155, image:"/images/icedamericano.jpeg" },
+        { id: 10, name: "Iced Caffe Latte", price:170, image:"/images/icedcafelatte.jpeg" },
+        { id: 11, name: "Iced Caffe Mocha", price:185, image:"/images/icedcoffemocha.jpeg" },
+        { id: 12, name: "Iced White Chocolate Mocha", price:185, image:"/images/icedwhitechocolatemocha.jpeg" },
+        { id: 13, name: "Iced Caramel Mocha", price:185, image:"/images/icedcaramellatte.jpeg" },
+        {id : 14, name: "Limonata", price:120, image:"/images/limonata.jpeg" },
+        {id : 15, name: "Ice Tea", price:100, image:"/images/icetea.jpeg" },
+        {id : 16, name: "Meyve Suyu", price:130, image:"/images/fruitjuice.jpeg" },
+        {id : 17, name: "Smoothie", price:150, image:"/images/smoothie.jpeg" },
+        {id: 18, name: "Kola", price:185, image:"/images/kola.jpeg" },
+        {id: 19, name: "Fanta", price:185, image:"/images/fanta.jpeg" },
+        {id: 20, name: "Sprite", price:185, image:"/images/sprite.jpeg" },
+        {id: 21, name: "Milkshake", price:185, image:"/images/milkshake.jpeg" },
+        {id: 22, name: "Bubble Tea", price:300, image:"/images/bubbletea.jpeg" },
+        {id: 23, name: "Matcha latte", price:450, image:"/images/coffee-milkshake.jpeg" },
+
+
+    ]
+},
+        {
+            category: "Tatlılar",
+            items: [
+                { id: 24, name: "Limonlu Cheesecake", price:215, image:"/images/lemoncheesecake.jpeg" },
+                { id: 25, name: "Brownie", price:220, image:"/images/brownie.jpeg" },
+                { id: 26, name: "Bella Vista", price:220, image:"/images/bellavista.jpeg" },
+                {id: 27, name: "San Sebastian", price:220, image:"/images/sansebastian.jpeg" },
+                {id: 28, name: "Cup Cake", price:150, image:"/images/cupcake.jpeg" },
+                {id: 29, name: "Waffle Combo", price:800, image:"/images/waffle.jpeg" }
+            ]
+        },
+        {
+            category: "Sandviçler",
+            items: [
+                { id: 30, name: "Sandviç Ev", price:190 ,image:"/images/evsandvic.jpeg" },
+                { id: 31, name: "Kahvaltı Sandviç", price:215, image:"/images/kahvaltisandvic.jpeg" },
+                { id: 32, name: "Tavuk Izgara Sandviç", price:215, image:"/images/tavuksandvic.jpeg" },
+                { id: 33, name: "Dana Janbon Sandviç", price:215, image:"/images/danajambon.jpeg" }
+            ]   
+        },
+        {
+    category: "Ana Yemekler",
+    items: [
+        { id: 34, name: "Izgara Tavuk", price: 295 , image:"/images/izgaratavuk.jpeg" },
+        { id: 35, name: "Köfte Tabağı", price: 310, image:"/images/kofte.jpeg" },
+        { id: 36, name: "Dana Antrikot", price: 420, image:"/images/danaantrikot.jpeg" },
+        { id: 37, name: "Fettuccine Alfredo", price: 280, image:"/images/fettucinialfredo.jpeg" },
+        { id: 38, name: "Napoli Pizza", price: 300, image:"/images/napolipizza.jpeg" }
+    ]
+},
+
+{
+    category:"Asya Mutfağı",
+    items:[
+        { id: 39, name: "Sushi California Roll", price: 350, image:"/images/californiaroll.jpeg" },
+        { id: 40, name: "Pad Thai", price: 320, image:"/images/padthai.jpeg" },
+        { id: 41, name: "Ramen", price: 300, image:"/images/ramen.jpeg" },
+        { id: 42, name: "Kung Pao Tavuk", price: 310, image:"/images/kungpao.jpeg" },
+        { id: 43, name: "Spring Roll", price: 150, image:"/images/springroll.jpeg" },
+        { id: 44, name: "Salmon Nigiri", price: 380, image:"/images/salmonnigiri.jpeg" },
+        { id: 45, name: "Tempura Roll", price: 360, image:"/images/tempuraroll.jpeg" }
+    ]
+},
+{
+    category:"Burger",
+    items:[
+        { id: 46, name: "Smoke Chili Burger", price: 400, image:"/images/smokechiliburger.jpeg" },
+        { id: 47  , name: "Şefin Özel Burgeri", price: 500, image:"/images/sefinozel.jpeg" },
+        { id: 48, name: "Vejetaryen Burger", price: 450, image:"/images/vegieburger.jpeg" },
+        { id: 49, name: "Tavuk Burger", price: 300, image:"/images/tavukburger.jpeg" },
+        { id: 50, name: "Cheeseburger", price: 330, image:"/images/cheeseburger.jpeg" },
+        { id: 51, name: "Meat Burger", price: 350, image:"/images/meatburger.jpeg" }
+    ]
+},
+{
+ category: "Salatalar",
+    items: [
+        { id: 52, name: "Sezar Salata", price: 180, image:"/images/sezarsalata.jpeg" },
+        { id: 53, name: "Yunan Salata", price: 160, image:"/images/yunansalata.jpeg" },
+        { id: 54, name: "Akdeniz Salata", price: 170, image:"/images/akdenizsalata.jpeg" },
+        { id: 55, name: "Ton Balıklı Salata", price: 190, image:"/images/tunasalata.jpeg" },
+        { id: 56, name: "Kinoa Salatası", price: 200, image:"/images/kinoasalata.jpeg" }
+    ]  
+},
+{
+    category: "Başlangıç",
+    items: [
+        { id: 57, name: "Çıtır Tavuk", price: 200, image:"/images/citirtavuk.jpeg" },
+        { id: 58, name: "Peynir Çubukları", price: 150, image:"/images/mozarella.jpeg" },
+        { id: 59, name: "Patates Kızartması", price: 200, image:"/images/fries.jpeg" },
+        { id: 60, name: "Soğan Halkaları", price: 150, image:"/images/onionring.jpeg" },
+        { id: 61, name: "Garlic Bread", price: 150, image:"/images/garlicbread.jpeg" }
+    ]   
+},
+{
+    category: "Kids Menu",
+    items: [
+        { id: 62, name: "Mini Pizza", price: 200, image:"/images/minipizza.jpeg" },
+        { id: 63, name: "Mini Kızartması", price: 200, image:"/images/fries.jpeg" },
+        { id: 64, name: "Dino Nuggets", price: 230, image:"/images/dinonuggets.jpeg" },
+        { id: 65, name: "Mini Burger", price: 200, image:"/images/miniburger.jpeg" },
+        { id: 66, name: "Maxi Burger", price: 300, image:"/images/cocukburger.jpeg" },
+        { id: 67, name: "Neşeli IceCream", price: 150, image:"/images/neselidondurma.jpeg" },
+        { id: 68, name: " Kitty Pasta", price: 299, image:"/images/hellokittypasta.jpeg" }
+    ]
+},
+{
+    category: "Combo Kasa Menüler",
+    items:[
+        { id: 69, name: "Heinz Combo Kasa 1", price: 3500, image:"/images/combobox.jpeg" },
+        { id: 70, name: "Tavuk Combo Kasa 2", price: 800, image:"/images/tavukcombo.jpeg" },
+        { id: 71, name: "Sweet Burger Combo Kasa 3", price: 2500, image:"/images/soslutavukcombo.jpeg" },
+        { id: 72, name: "Friends Combo Kasa 4", price: 600, image:"/images/friendscombo.jpeg" },
+        { id: 73, name: "Hot Dog Combo Kasa 5", price: 2000, image:"/images/hotdog.jpeg" }
+         ]
+},
+{ 
+    category: "Kahvaltı",
+    items:[
+        { id: 74, name: "Serpme  Kahvaltı", price: 2000, image:"/images/serpmekahvalti.jpeg" },
+        { id: 75, name: "Mini Serpme Kahvaltı", price: 500, image:"/images/minikahvalti.jpeg" },
+        { id: 76, name: "Patates Tabağı", price: 300, image:"/images/kahvaltipatatesi.jpeg" },
+        { id: 77, name: "Simit Tabağı", price: 400, image:"/images/simittabagi.jpeg" },
+        { id: 78, name: "Menemen", price: 200, image:"/images/Menemen.jpeg" },
+        { id: 79, name: "Sucuklu Yumurta", price: 200, image:"/images/sucukluyumurta.jpeg" },
+        { id: 80, name: "Peynir Tabağı", price: 300, image:"/images/peynirtabagi.jpeg" }            
+        
+
+    ]
+}
+    
+]);
+});
+
+/* ---------------- ORDER POST ---------------- */
+app.post('/order', (req,res)=>{
+    console.log("POST GELDI 🔥");
+    console.log("Gelen veri:", req.body);
+
+    const order = req.body;
+    order.time = Date.now();
+
+    orders.push(order);
+
+    console.log("Guncel orders:", orders);
+
+    res.json({status:"ok"});
+});
+
+
+/* ---------------- ORDER GET ---------------- */
+app.get('/orders', (req,res)=>{
+    res.json(orders);
+});
+
+/* ---------------- ROOT ---------------- */
+app.get('/', (req, res) => {
+    res.send('Server calisiyor 🚀');
+});
+app.delete('/delete/:index', (req,res)=>{
+    const index = req.params.index;
+    orders.splice(index,1);
+    res.json({status:"deleted"});
+});
+
+/* ---------------- LISTEN (EN SON) ---------------- */
+app.listen(3000, () => {
+    console.log('Server 3000 portunda calisiyor');
+});
